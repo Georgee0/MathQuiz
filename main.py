7edf4e36fb71de7582efbdbb8010555194618850
George Moses <67199528+Georgee0@users.noreply.github.com>
2021-12-11 04:30:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# Passing a custom validation input function: inputCustom()
-'''
-import pyinputplus as pyip
-from pyinputplus import inputCustom
-print('Enter number to add to 10')
-#response = pyip.inputCustom(addTenNum) 
-def addTenNum(numbers):
-    numbersList = list(numbers)
-    for i, digit in enumerate(numbersList):
-        numbersList[i] = int(digit)
-    if sum(numbersList) != 10:
-        raise Exception('The digits must add up to 10 not %s' %(sum(numbersList))) 
-    return int(numbers)           
-response = pyip.inputCustom(addTenNum)
-print(response)    
-'''
-# How to keep am idiot busy 
-'''
-import pyinputplus as pyip
-while True:
-    prompt = 'Want to know how to keep an idiot busy for quite a long time?\n'
-    response = pyip.inputYesNo(prompt)
-    if response == 'yes':
-        continue
-    elif response == 'no':
-        break
-print('Thank you for you for your time')
-'''
 # Multiplication Quiz
 import pyinputplus as pyip
 import random, time
